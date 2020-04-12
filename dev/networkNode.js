a6f3be226bf6488cc38d7ca23659bcba608a80ad
Yachint <yachint.yadav@gmail.com>
2020-04-12 19:25:27 +0530

    MAJOR VERSION UPDATE

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,16 +1,14 @@
 var express = require('express');
-const crypto = require('crypto');
 const process = require('process');
-const hash = require('./hash');
 var app = express();
 const blockchain = require('./main');
 const uuid = require('uuid');
 const nodeAddress = uuid.v1().split('-').join("");
 var bodyParser = require('body-parser');
 const scabChain = new blockchain();
-const rp = require('request-promise');
 const port = process.argv[2];
-const IpfsExtract = require('./IPFS_ExtractTest');
+
+
 //-----ROUTES----------------------------------------
 const displayChain = require('./Routes/displayChain');
 const transactions = require('./Routes/Transactions/transactions');
@@ -34,61 +32,13 @@ app.get('/', function(req,res){
 
 app.use('/blockchain', displayChain);
 app.use('/transaction', transactions);
-
 app.use('/compress', compress);
-
-// app.post('/compress/accept', function(req, res){
-
-// })
-
-// app.post('/compress/check',async function(req, res){
-    
-// });
-
-// app.get('/compress/approval', async function(req,res){
-
-// });
-
 app.use('/mine',mine);
-
-// app.get('/mine', function(req, res){
-    
-// });
-
 app.use('/receive-new-block',receiveNewBlock);
-
-// app.post('/receive-new-block', function(req,res){
-// });
-
 app.use('/update-DHT', updateDHT);
-
-// app.post('/update-DHT', function(req,res){
-
-// });
-
 app.use('/consensus', consensus);
-
-// app.get('/consensus', function(req, res){
-   
-// });
-
 app.use('/register-and-broadcast-node',regAndBdcast);
-
-// app.post('/register-and-broadcast-node', function(req,res){
-
-
-// });
-
 app.use('/register-node',registerNode);
-
-// app.post('/register-node', function(req,res){
-
-// });
-
-// app.post('/register-nodes-bulk', function(req,res){
-
-// });
-
 app.use('/register-nodes-bulk',registerNodesBulk);
 
 app.listen(port, function(){
